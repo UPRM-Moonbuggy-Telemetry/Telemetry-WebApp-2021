288b1d5bf9284133514de78dc79443324bc09c02
FherRodz <fher.rodriguez@upr.edu>
2022-02-08 16:52:40 -0500

    fixed some more grid dynamic interactions

--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -19,26 +19,31 @@ export default function Cards() {
     const ResponsiveGridLayout = WidthProvider(Responsive)
 
     const layouts = {lg:[
-        {i: 'GPS', x: 1, y: 0, w: 2, h: 4},
-        {i: 'b', x: 3, y: 0, w: 2, h: 4},
-        {i: 'c', x: 5, y: 0, w: 2, h: 4},
-        {i: 'd', x: 7, y: 0, w: 2, h: 4},
+        {i: 'GPS', x: 0, y: 1, w: 1, h: 1, isDraggable: true, isBounded: true},
+        {i: 'STRAIN CHART', x: 0, y: 0, w: 1, h: 1},
+        {i: 'c', x: 2, y: 0, w: 1, h: 1},
+        // {i: 'd', x: 3, y: 0, w: 1, h: 1},
 
 
         // {i: 'b', x: 1, y: 0, w: 3, h: 8, minW: 3, maxH: 2},
         // {i: 'c', x: 3, y: 0, w: 3, h: 8, minW: 3, minH: 8, maxH: 10},
         // {i: 'd', x: 4, y: 0, w: 3, h: 8, minW: 3, minH: 8, maxH: 10},
         // {i: 'e', x: 0, y: 1, w: 3, h: 8, minW: 3, minH: 8, maxH: 10}
+    ], md:[
+        {i: 'b', x: 0, y: 0, w: 1, h: 1},
+        {i: 'c', x: 1, y: 0, w: 1, h: 1},
+        {i: 'd', x: 0, y: 0, w: 1, h: 1},
+        {i: 'e', x: 1, y: 0, w: 1, h: 1}
     ]
     };
 
     return (
         <div style={{maxHeight:'max-content'}}>
-            <ResponsiveGridLayout className="layout" layout={layouts} isBounded={true} compactType={"hoizontal"}
-            breakpoints={{lg: 2000}}
-            cols={{lg: 12, md: 3}} 
+            <ResponsiveGridLayout className="layout"  layouts={layouts} isBounded={true} compactType={"hoizontal"}
+            breakpoints={{lg: 1201, md:1200}}
+            cols={{lg: 3, md: 2}} 
             maxRows={2}
-            rowHeight={200} 
+            rowHeight={400} 
             margin={[10,25]}>
                 {layouts['lg'].map((i) => (
                     <section key={i.i} className="block">
