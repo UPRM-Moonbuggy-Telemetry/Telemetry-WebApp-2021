8026f70def8ae18719212118e9b00f726b5abff8
Luis F. Quiles Ruiz <luis.quiles1@upr.edu>
2023-03-07 21:30:59 -0400

    Fixed endpoints for env on apis and error handling.

--- client/src/api/batteryStatus.js
+++ client/src/api/batteryStatus.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 
 export const getBatteryStatus = () => {
     return axios
-        .get(`https://app-moonbuggy.herokuapp.com/api/dataEntry/battery`)
+        .get(`${process.env.REACT_APP_ENDPOINT}/api/dataEntry/battery`)
         .then( response => {
             return response.data
         })
-        .catch(err => {
-            return err.response
+        .catch(error => {
+            return error;
         })
 }
--- client/src/api/gpsLocation.js
+++ client/src/api/gpsLocation.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 
 export const getGpsLocation = () => {
     return axios
-        .get(`https://app-moonbuggy.herokuapp.com/api/dataEntry/location`)
+        .get(`${process.env.REACT_APP_ENDPOINT}/api/dataEntry/location`)
         .then( response => {
             return response.data
         })
-        .catch(err => {
-            return err.response
+        .catch(error => {
+            return error;
         })
 }
